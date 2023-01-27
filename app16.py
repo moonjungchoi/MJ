3e4e2f0b0ccde34ff41c355c9eb2ce6f623c0787
moonjungchoi <123626037+moonjungchoi@users.noreply.github.com>
2023-01-27 17:06:40 +0900

    Update app16.py

--- app16.py
+++ app16.py
@@ -3,12 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-#money = pd.read_csv("money_data7.csv")
-
-#st.sidebar.success("Select a demo above.")
 
 def  plotting_demo():
-    money = pd.read_csv("money_data7.csv")
+    
+    uploaded_file = st.file_uploader("Choose a file")
+
+    money=pd.read_csv(uploaded_file)
+   # money = pd.read_csv("money_data7.csv")
+
     option = st.selectbox(
         'How would you like to choice year ?',
         ('2020', '2021', '2022'))
@@ -44,12 +46,16 @@ def  plotting_demo():
 
     st.pyplot(fig)
     st.dataframe(money)
+       
 
 with st.form(key ='Form1'):
     with st.sidebar:
         
-        select_language = st.sidebar.radio('What do you want ?', ('line graph', 'bar graph', 'pie graph', 'histogram graph', 'corr graph','word cloud', 'box graph'))
+        select_language = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie'))
         
         
-if select_language =='line graph':        
-    plotting_demo()      
+if select_language =='line':           
+    try:
+          plotting_demo()  
+    except:      
+          pass
