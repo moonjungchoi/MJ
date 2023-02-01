fe5c91cb2c6864f9f81f638f0af1295e3f26ed40
moonjungchoi <123626037+moonjungchoi@users.noreply.github.com>
2023-02-01 17:17:11 +0900

    Update app16.py

--- app16.py
+++ app16.py
@@ -24,13 +24,13 @@ def  plotting_demo():
     fig, ax = plt.subplots(2,2, figsize=(12,8))
 
     plt.subplot(221)
-    plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+    plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='purple' , marker='o'     ) 
     plt.xticks(tuple(money['A_MONTH']) )
     plt.title('America rate')
 
 
     plt.subplot(222)
-    plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
+    plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='pink' , marker='o'     ) 
     plt.xticks(tuple(money['A_MONTH']) )
     plt.title('Korea rate')
 
